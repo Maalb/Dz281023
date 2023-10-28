96642c137b4e2c573281435ae6d99be33eba73a4
Maalb <110936451+Maalb@users.noreply.github.com>
2023-10-28 18:49:54 +0300

    Сравнение массивов

--- src/MasEquals.java
+++ src/MasEquals.java
@@ -1,2 +1,26 @@
 public class MasEquals {
+    public static void main(String[] args) {
+        int[] mas1 ={1, 2, 3, 5, 7};
+        int[] mas2={1, 2, 3, 5, 7};
+        System.out.println(ravnySassives(mas1, mas2));
+    }
+    static boolean ravnySassives(int[] m1, int[] m2){
+        boolean ravLenght = m1.length==m2.length ? true : false;
+        if (ravLenght==true) {
+            int ravUnit = 0;
+            for (int i = 0; i < m1.length; i++) {
+                if (m1[i] == m2[i]) {
+                    ravUnit += 1;
+                }
+            }
+            if (ravUnit == m1.length) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }else {
+            return false;
+        }
+    }
 }
