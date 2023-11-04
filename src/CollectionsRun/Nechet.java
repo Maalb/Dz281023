909258f8cf77cb05f441ddfbf080bf7d390ccdcf
Maalb <110936451+Maalb@users.noreply.github.com>
2023-11-04 22:11:13 +0300

    Игра Герой

--- src/CollectionsRun/Nechet.java
+++ src/CollectionsRun/Nechet.java
@@ -13,12 +13,15 @@ public class Nechet {
         mix.add(6);
         mix.add(7);
         mix.add(8);
+        mix.add(8);
 
         System.out.println(mix);
+        System.out.println(countUnique(mix));
         System.out.println(mix1(mix));
+
     }
 
-    static ArrayList<Integer> mix1 (ArrayList<Integer> mix1) {
+    static ArrayList<Integer> mix1 (ArrayList<Integer> mix1) { // метод заданийе 1
         for (int i = 0; i < mix1.size(); i++) {
             if (mix1.get(i) % 2 == 0) {
                 mix1.remove(mix1.get(i));
@@ -26,4 +29,17 @@ public class Nechet {
         }
         return mix1;
     }
+
+    static int countUnique(ArrayList<Integer> mix1) { // метод задание 2
+        List mix2 = new ArrayList<>() {
+        };
+        for (int i = 0; i < mix1.size(); i++) {
+            if (mix2.contains(mix1.get(i))) {
+            } else {
+                mix2.add(mix1.get(i));
+            }
+        }
+        return mix2.size();
+    }
+
 }
