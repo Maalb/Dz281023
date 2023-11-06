cb21f167b7f90a8c523bee824a37d44f68507237
Maalb <110936451+Maalb@users.noreply.github.com>
2023-11-06 18:39:47 +0300

    Игра Герой

---
+++ src/CollectionsRun/TextFrequancy.java
@@ -0,0 +1,35 @@
+
+package CollectionsRun;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.*;
+
+public class TextFrequancy {
+    public static void main(String[] args) {
+        String s = "Вот уже пять лет, как его не расщепляли на атомы. " +
+                "Вот уже четыре года (со дня расставания с Фордом Префектом) " +
+                "Артур в общем-то ни одной души не видел, а потому и оскорблениям " +
+                "больше не подвергался. За исключением одного случая. " +
+                "Это произошло как-то весенним вечером года два тому назад.";
+
+        System.out.println(words(s));
+    }
+    static Map words(String bezZnakov){
+        Map<String, Integer> words1 = new HashMap<>();
+        String[] ss = bezZnakov.split(" ");
+        for (String word: ss) {
+            if (words1.containsKey(word)){
+                words1.put(word, words1.get(word) +1);
+            } else {
+                words1.put(word, 1);
+            }
+        }
+        return words1;
+    }
+
+    static String bezZnakov(String sss){
+        String ss = sss.replaceAll("[^A-Za-zА-Яа-я0-9]", " ");
+        return ss;
+    }
+}
